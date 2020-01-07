c90a3e5643fd99c9ad0268b4db56a5d1725a86d8
liang <978572082@qq.com>
2020-01-07 09:18:58 +0800

    add recommend category seeMore

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,23 @@
 <template>
-	<view>
-		微信读书
-	</view>
+	<div class="indexContainer">
+		微信读书11111
+		<Category/>
+		<hr>
+		<Recommend/>
+	</div>
+	
 </template>
 
 <script>
+	
+	import Recommend from '../../components/recommend/recommend.vue'
+	import Category from '../../components/category/category.vue'
 	export default {
+		components:{
+			Recommend,
+			Category
+		},
+	
 
 	}
 </script>
