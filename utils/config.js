406a07a15ff70644b5ccf14619132e1bb38df34f
WANG <amyamelia9527@gmail.com>
2020-01-07 09:51:23 +0800

    serachPage

---
+++ utils/config.js
@@ -0,0 +1,4 @@
+export default {
+	host:"http://localhost:3000",
+	// host:"http://wjy-api.bj-srv-01.rainhan.cn",
+}
---
+++ utils/request.js
@@ -0,0 +1,16 @@
+import config from "./config.js"
+export default function(url,data={},method="GET"){
+	return new Promise((resolve,reject)=>{
+		uni.request({
+			url:config.host+url,
+			data,
+			method,
+			success(res) {
+				resolve(res.data)
+			},
+			fail() {
+				console.log("请求失败")
+			}
+		})
+	})
+}
